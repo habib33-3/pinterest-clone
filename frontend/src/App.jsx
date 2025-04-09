19d4864de75d839ecd927b98ee544c7582fbf768
habib33-3 <habib.rahman0330@gmail.com>
2025-04-10 01:00:39 +0600

    sidebar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,50 +1,18 @@
-import { useState } from "react";
-
 import "./App.css";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import LeftBar from "./components/LeftBar/LeftBar";
+import Gallery from "./components/gallery/gallery";
+import TopBar from "./components/topBar/topBar";
 
-function App() {
-  const [count, setCount] = useState(0);
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a
-          href="https://vite.dev"
-          target="_blank"
-        >
-          <img
-            src={viteLogo}
-            className="logo"
-            alt="Vite logo"
-          />
-        </a>
-        <a
-          href="https://react.dev"
-          target="_blank"
-        >
-          <img
-            src={reactLogo}
-            className="logo react"
-            alt="React logo"
-          />
-        </a>
+    <div className="app">
+      <LeftBar />
+      <div className="content">
+        <TopBar />
+        <Gallery />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   );
-}
+};
 
 export default App;
