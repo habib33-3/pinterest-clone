94de0ac9170f70544018157f54c9a9117d284f4e
habib33-3 <habib.rahman0330@gmail.com>
2025-04-11 22:58:38 +0600

    comments

--- frontend/src/components/comments/Comments.jsx
+++ frontend/src/components/comments/Comments.jsx
@@ -1,11 +1,54 @@
+import { useState } from "react";
 
+import EmojiPicker from "emoji-picker-react";
+
+import Image from "../Image/Image";
+import "./comments.css";
 
 const Comments = () => {
-    return (
-        <div>
-            
+  const [open, setOpen] = useState(false);
+
+  return (
+    <div className="comments">
+      <div className="commentList">
+        <span className="commentCount">5 Comments</span>
+        <div className="comment">
+          <Image
+            path={"/general/noAvatar.png"}
+            alt=""
+          />
+          <div className="commentContent">
+            <div className="commentUsername">John Doe</div>
+            <p className="commentText">
+              lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi,
+              nesciunt.
+            </p>
+            <span className="commentTime">1h ago</span>
+          </div>
         </div>
-    );
+      </div>
+
+      <form className="commentForm">
+        <input
+          type="text"
+          placeholder="Add a comment"
+          name="comment"
+          id="comment"
+        />
+        <div
+          onClick={() => setOpen(!open)}
+          className="emoji"
+        >
+          😊
+        </div>
+        {open && (
+          <div className="emojiPicker">
+            <EmojiPicker />
+          </div>
+        )}
+      </form>
+    </div>
+  );
 };
 
 export default Comments;
