527ed7c34894ac1f8dd9dcd0b379ffc69995e208
habib33-3 <habib.rahman0330@gmail.com>
2025-04-12 00:47:40 +0600

    profile page

---
+++ frontend/src/components/collections/Collections.jsx
@@ -0,0 +1,92 @@
+import Image from "../Image/Image";
+import "./collections.css";
+
+const Collections = () => {
+  return (
+    <div className="collections">
+      <div className="collection">
+        <Image
+          path="/pins/pin1.jpeg"
+          alt=""
+        />
+        <div className="collectionInfo">
+          <h1>Minimalist Bedrooms</h1>
+          <span>12 pins . 1w</span>
+        </div>
+      </div>
+      <div className="collection">
+        <Image
+          path="/pins/pin1.jpeg"
+          alt=""
+        />
+        <div className="collectionInfo">
+          <h1>Minimalist Bedrooms</h1>
+          <span>12 pins . 1w</span>
+        </div>
+      </div>
+  
+      <div className="collection">
+        <Image
+          path="/pins/pin1.jpeg"
+          alt=""
+        />
+        <div className="collectionInfo">
+          <h1>Minimalist Bedrooms</h1>
+          <span>12 pins . 1w</span>
+        </div>
+      </div>
+      <div className="collection">
+        <Image
+          path="/pins/pin1.jpeg"
+          alt=""
+        />
+        <div className="collectionInfo">
+          <h1>Minimalist Bedrooms</h1>
+          <span>12 pins . 1w</span>
+        </div>
+      </div>
+      <div className="collection">
+        <Image
+          path="/pins/pin1.jpeg"
+          alt=""
+        />
+        <div className="collectionInfo">
+          <h1>Minimalist Bedrooms</h1>
+          <span>12 pins . 1w</span>
+        </div>
+      </div>
+      <div className="collection">
+        <Image
+          path="/pins/pin1.jpeg"
+          alt=""
+        />
+        <div className="collectionInfo">
+          <h1>Minimalist Bedrooms</h1>
+          <span>12 pins . 1w</span>
+        </div>
+      </div>
+      <div className="collection">
+        <Image
+          path="/pins/pin1.jpeg"
+          alt=""
+        />
+        <div className="collectionInfo">
+          <h1>Minimalist Bedrooms</h1>
+          <span>12 pins . 1w</span>
+        </div>
+      </div>
+      <div className="collection">
+        <Image
+          path="/pins/pin1.jpeg"
+          alt=""
+        />
+        <div className="collectionInfo">
+          <h1>Minimalist Bedrooms</h1>
+          <span>12 pins . 1w</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Collections;
