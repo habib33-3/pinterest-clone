b30f7ec18740c7d1998977981749a59fc0b6b3a9
habib33-3 <habib.rahman0330@gmail.com>
2025-04-12 20:37:49 +0600

    search and infinite scroll

--- backend/controllers/pins.controller.js
+++ backend/controllers/pins.controller.js
@@ -1,6 +1,36 @@
 import Pin from "../models/pins.model.js";
 
-export const getPins=async(req,res)=>{
-    const pins=await Pin.find();
-    res.status(200).send(pins);
-}
+export const getPins = async (req, res) => {
+  const pageNumber = Number(req.query.cursor) || 0;
+  const search = req.query.search || "";
+
+  const LIMIT = 21;
+
+  const pins = await Pin.find(
+    search
+      ? {
+          $or: [
+            {
+              title: {
+                $regex: search,
+                $options: "i",
+              },
+            },
+            {
+              tags: {
+                $in: search,
+              },
+            },
+          ],
+        }
+      : {}
+  )
+    .limit(LIMIT)
+    .skip(LIMIT * pageNumber);
+
+  const hasNextPage = pins.length === LIMIT;
+
+  res
+    .status(200)
+    .send({ pins, nextCursor: hasNextPage ? pageNumber + 1 : null });
+};
