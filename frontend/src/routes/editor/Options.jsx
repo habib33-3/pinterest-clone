b0f24be5f491908f4a4a1cc00678ea2ce686d2bf
habib33-3 <habib.rahman0330@gmail.com>
2025-04-14 23:09:03 +0600

    text options|

--- frontend/src/routes/editor/Options.jsx
+++ frontend/src/routes/editor/Options.jsx
@@ -1,7 +1,60 @@
-import React from "react";
+import { useState } from "react";
+
+import { HexAlphaColorPicker } from "react-colorful";
+
+import { useEditorStore } from "../../utils/editorStore";
 
 const Options = () => {
-  return <div></div>;
+  const { selectedLayer, setTextOptions, textOptions } = useEditorStore();
+
+  const [isColorPickerOpen, setIsColorOpen] = useState(false);
+
+  return (
+    <div className="options">
+      {selectedLayer === "text" ? (
+        <div className="">
+          <div className="editingOption">
+            <span>Font Size</span>
+            <input
+              type="number"
+              name=""
+              id=""
+              value={textOptions.fontSize}
+              onChange={(e) =>
+                setTextOptions({ ...textOptions, fontSize: e.target.value })
+              }
+            />
+          </div>
+          <div className="editingOption">
+            <span>Color</span>
+            <div className="textColor">
+              <div
+                className="colorPreview"
+                style={{ backgroundColor: textOptions.color }}
+                onClick={() => setIsColorOpen(!isColorPickerOpen)}
+              >
+                {isColorPickerOpen && (
+                  <div className="colorPicker">
+                    <HexAlphaColorPicker
+                      color={textOptions.color}
+                      onChange={(color) =>
+                        setTextOptions({
+                          ...textOptions,
+                          color,
+                        })
+                      }
+                    />
+                  </div>
+                )}
+              </div>
+            </div>
+          </div>
+        </div>
+      ) : (
+        <div className=""></div>
+      )}
+    </div>
+  );
 };
 
 export default Options;
