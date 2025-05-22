fb186e0d1d23801075df9a628798b9a596211147
habib33-3 <habib.rahman0330@gmail.com>
2025-05-22 20:57:25 +0600

    Refactor cookie name usage

--- apps/backend/src/middlewares/verifyAuth.middleware.ts
+++ apps/backend/src/middlewares/verifyAuth.middleware.ts
@@ -4,10 +4,13 @@ import { StatusCodes } from "http-status-codes";
 
 import { verifyToken } from "@/lib/jwt";
 
+import { COOKIE_NAME } from "@/shared/constants";
+
 import { prisma } from "@/db/prisma";
 
 const verifyAuth = async (req: Request, res: Response, next: NextFunction) => {
-    const token = req.cookies.token;
+    // eslint-disable-next-line security/detect-object-injection
+    const token = req.cookies[COOKIE_NAME];
 
     if (!token) {
         res.status(StatusCodes.UNAUTHORIZED).json({
@@ -39,6 +42,7 @@ const verifyAuth = async (req: Request, res: Response, next: NextFunction) => {
             id: user.id,
             email: user.email,
         };
+
         next();
         // eslint-disable-next-line @typescript-eslint/no-explicit-any
     } catch (err: any) {
